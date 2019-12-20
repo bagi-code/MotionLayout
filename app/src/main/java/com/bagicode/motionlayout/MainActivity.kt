bbebf21cec3d5dfcc38c4c1a72a999e87e84f67f
Bagicode <bagicode@gmail.com>
2019-12-20 20:09:09 +0700

    Initial commit

---
+++ app/src/main/java/com/bagicode/motionlayout/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bagicode.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main_start.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_start)
+    }
+}
